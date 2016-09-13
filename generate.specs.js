9728d3cfba175b2d8b344cd057feea5c9872cc41
Dave Kerr <dwmkerr@gmail.com>
2016-09-13 14:51:51 +0800

    A bit more explicit with the tests

--- generate.specs.js
+++ generate.specs.js
@@ -5,7 +5,6 @@ describe('Find Icon Sets', () => {
   it('should be able to find the iOS iconsets', () => {
     return findIconSets('./').then(iconSets => {
       expect(iconSets).to.include('test/ReactNativeIconTest/ios/ReactNativeIconTest/Images.xcassets/AppIcon.appiconset');
-      expect(iconSets.length).to.equal(2); // there's also an iconset in a node_modules folder...
     });
   });
 });
