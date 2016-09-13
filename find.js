5e6bcbfcc8772d07571fd419ce0c797dd044b01d
Dave Kerr <dwmkerr@gmail.com>
2016-09-13 14:46:12 +0800

    Still work in progress, setting up CI to help a bit with coverage etc

---
+++ find.js
@@ -0,0 +1,41 @@
+//  find.js
+//
+//  Find stuff in a file system. Pass a predicate which will
+//  be given a fs.stat, e.g:
+//
+//  find('.', (stat) => stat.isDirectory());
+//
+//  Inspired by: http://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
+
+const fs = require('fs');
+const path = require('path');
+
+function walk(dir, existingResults, predicate, done) {
+  const results = existingResults || [];
+  fs.readdir(dir, (err, list) => {
+    var pending = list.length;
+    if (err || pending === 0) return done(err, results);
+    list.forEach((file) => {
+      file = path.resolve(dir, file);
+      fs.stat(file, (err, stat) => {
+        if (predicate(file, stat)) results.push(file); 
+        if (stat && stat.isDirectory()) {
+          walk(file, results, predicate, (err, res) => {
+            if (!--pending) done(null, results);
+          });
+        } else {
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+}
+
+module.exports = function find(root, predicate) {
+  return new Promise((resolve, reject) => {
+    walk(root, [], predicate, (err, files) => {
+      if(err) return reject(err);
+      return resolve(files.map(f => path.relative(root, f)));
+    });
+  });
+};
---
+++ generate.js
@@ -0,0 +1,50 @@
+const find = require('./find');
+
+function findIconSets() {
+  return find('./', (file, stat) => {
+    return file.match(/AppIcon.appiconset/) && stat.isDirectory();
+  });
+}
+
+//  Generate xCode icons given an iconset folder.
+function generateIconSetIcons(iconSet) {
+  console.log(`Generating icons for iconset ${path.relative('./', iconSet)}...`);
+
+  //  We've got the iconset folder. Get the contents Json.
+  var contents = require(path.join(iconSet, 'Contents.json'));
+  contents.images = [];
+
+  //  Generate each image in the full icon set, updating the contents.
+  return Promise.all(fullIconSet.images.map(image => {
+    const targetName = `icon-${image.size}-${image.scale}.png`;
+    const targetPath = path.join(iconSet, targetName);
+    console.log(`Generating ${targetName} from icon.png...`);
+    return generateIcon("icon.png", targetPath, image.size)
+      .then(() => {
+        contents.images.push({
+          size: image.size,
+          idiom: image.idiom,
+          scale: image.scale,
+          filename: targetName
+        });
+      });
+  }));
+}
+
+function generate() {
+  return findIconSets()
+    .then(iconSets => {
+      return Promise.all(iconSets.map(generateIconSetIcons));
+    });
+}
+
+const fullIconSet = {
+  images: [{
+    size: 29, idiom: "iphone", scale: "2x"
+  }]
+};
+
+module.exports = {
+  findIconSets,
+  generate
+};
