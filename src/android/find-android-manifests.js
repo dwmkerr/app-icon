aff4c4e2c0223a8ab6638fb3cb1f9dd56e8a8c1c
Dave Kerr <dwmkerr@gmail.com>
2017-03-09 15:33:46 +0700

    Cleaned up the source code structure

---
+++ src/android/find-android-manifests.js
@@ -0,0 +1,15 @@
+const find = require('../utils/find');
+
+//  Given a search root, finds all iOS iconsets.
+function findAndroidManifests(searchRoot) {
+  return find(searchRoot, (file, stat) => {
+    //  Exclude: node modules and android build intermediates.
+    if (file.match(/node_modules/)) return false;
+    if (file.match(/\/build\//)) return false;
+
+    //  Only grab the manifest file...
+    return file.match(/AndroidManifest.xml/) && !stat.isDirectory();
+  });
+}
+
+module.exports = findAndroidManifests;
