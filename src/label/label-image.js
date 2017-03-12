c0617fb1757c58ca4a0d9da8aa19791376df050c
Dave Kerr <dwmkerr@gmail.com>
2017-03-12 23:14:52 +0800

    Removed unneeded files from the repo, ensured test images are saved as CircleCI artifacts

--- src/label/label-image.js
+++ src/label/label-image.js
@@ -2,7 +2,7 @@ const childProcess = require('child_process');
 const getImageWidth = require('./get-image-width');
 
 //  Use imagemagick to label an image. Gravity should be 'north' or 'south'.
-function label(input, output, label, gravity) {
+function caption(input, output, label, gravity) {
   return new Promise((resolve, reject) => {
     //  Get the image width.
     getImageWidth(input)
@@ -30,12 +30,12 @@ function label(input, output, label, gravity) {
 //  Single function to label an image (optionally top and bottom).
 module.exports = function labelImage(input, output, top, bottom) {
   if (top && bottom) {
-    return label(input, output, top, 'north')
-      .then(() => label(output, output, bottom, 'south'));
+    return caption(input, output, top, 'north')
+      .then(() => caption(output, output, bottom, 'south'));
   } else if (top) {
-    return label(input, output, top, 'north');
+    return caption(input, output, top, 'north');
   } else if (bottom) {
-    return label(input, output, bottom, 'south');
+    return caption(input, output, bottom, 'south');
   }
 
   throw new Error("When labeling an image, a 'top' or 'bottom' must be specified.");
