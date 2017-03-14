ffe06f4410677fb323443aff6f699cf4faa10a70
Dave Kerr <dwmkerr@gmail.com>
2017-03-14 22:15:56 +0700

    Trying a hunch - deconstructed function parameters are breaking istanbul...

--- src/imagemagick/call-imagemagick.specs.js
+++ src/imagemagick/call-imagemagick.specs.js
@@ -4,21 +4,24 @@ const callImagemagick = require('./call-imagemagick');
 describe('call-imagemagick', () => {
   it('should be able to call imagemagick and get the installed version', () => {
     return callImagemagick('identify -version')
-      .then(({ stdout }) => {
+      .then((results) => {
+        const { stdout } = results;
         expect(stdout).to.match(/Version: ImageMagick/);
       });
   });
 
   it('should return a sensible error if the command has bad parameters', () => {
     return callImagemagick('identify no-file.png')
-      .catch(({ err }) => {
+      .catch((results) => {
+        const { err } = results;
         expect(err.message).to.match(/unable to open image/);
       });
   });
 
   it('should return a sensible error if the command does not exist', () => {
     return callImagemagick('there-is-no-way-this-is-an-installed-command')
-      .catch(({ err }) => {
+      .catch((results) => {
+        const { err } = results;
         expect(err.message).to.match(/not found/);
       });
   });
