90203232199c4a8daa11fb7e2e2b58783b9d2158
Dave Kerr <dwmkerr@gmail.com>
2017-06-02 22:28:51 +0800

    feature: added platforms flag. closes #32.

--- src/generate.specs.js
+++ src/generate.specs.js
@@ -1,14 +1,19 @@
 const expect = require('chai').expect;
 const generate = require('./generate');
 
-const sourceIcon = 'icon.png';
-
 describe('generate', () => {
-  it('should be able to generate React Native icons', () => {
+  it('should be able to generate test app icons', () => {
+    const parameters = {
+      sourceIcon: 'icon.png',
+      searchPath: './',
+    };
+
     //  Delete all of the files we're expecting to create, then generate them.
-    return generate(sourceIcon, './test/ReactNativeIconTest').then((results) => {
+    return generate(parameters).then((results) => {
       //  TODO: Check we found the manifests etc etc
       expect(results).not.to.equal(null);
+      expect(results.iconsets.length).to.equal(3);
+      expect(results.manifests.length).to.equal(4);
     });
   });
 });
