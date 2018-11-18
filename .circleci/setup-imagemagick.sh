9d5aa000f6c92ae185d2986eae5423378b8b4863
Dave Kerr <dwmkerr@gmail.com>
2018-11-18 18:32:57 +0900

    build: fix path to imagemagick

--- .circleci/setup-imagemagick.sh
+++ .circleci/setup-imagemagick.sh
@@ -6,7 +6,7 @@ sudo apt remove --purge imagemagick
 
 # Install IM 7.
 version="ImageMagick-7.0.8-3"
-cp ./test/downloads/${version}.tar.gz .
+cp ${version}.tar.gz .
 tar xzf ${version}.tar.gz
 cd ${version}
 touch configure
