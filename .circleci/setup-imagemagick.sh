cddaa3239ee16685489969703ccdd50981113fcc
Dave Kerr <dwmkerr@gmail.com>
2018-11-18 17:37:06 +0900

    build: explicitly install imagemagick

---
+++ .circleci/setup-imagemagick.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# Remove any installed image magick.
+sudo apt remove --purge imagemagick
+
+# Install IM 7.
+version="ImageMagick-7.0.8-3"
+cp ./test/downloads/${version}.tar.gz .
+tar xzf ${version}.tar.gz
+cd ${version}
+touch configure
+./configure
+make
+sudo make install
+sudo ldconfig /usr/local/lib
+convert -version
+cd ..
