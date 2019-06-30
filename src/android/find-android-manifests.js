07b971571c6cc57ec95a6f9dd623f673d63cb155
Dave Kerr <dwmkerr@gmail.com>
2019-06-30 17:11:51 +0800

    feat(android): improved handling of 'build' folder
    If your project has folder named 'build' as part of its path, manifests
    will still be correctly found. Any _subfolders_ which are named 'build'
    will still be excluded.
    
    Fixes #109.

--- src/android/find-android-manifests.js
+++ src/android/find-android-manifests.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const debug = require('debug')('app-icon');
 const find = require('../utils/find');
 
 //  Create a regexp to exclude node modules and build intermediates. The build folder rex
@@ -8,12 +9,34 @@ const rexBuildFolder = new RegExp(`${path.normalize('/build/').replace(/\\/g, '\
 
 //  Given a search root, finds all Android manifests.
 module.exports = async function findAndroidManifests(searchRoot) {
+  debug(`searching ${searchRoot} for android manifests`);
+  const absoluteSearchRoot = path.resolve(searchRoot);
   return find(searchRoot, (file, stat) => {
-    //  Exclude: node modules and android build intermediates.
-    if (file.match(rexNodeModules)) return false;
-    if (file.match(rexBuildFolder)) return false;
+    //  Anything which is a directory or does not end with 'AndroidManifest.xml'
+    //  we can immediately ignore.
+    if (!file.match(/AndroidManifest.xml/) || stat.isDirectory()) {
+      //  No point debugging this, we'll just blat the screen with too much output.
+      return false;
+    }
 
-    //  Only grab the manifest file...
-    return file.match(/AndroidManifest.xml/) && !stat.isDirectory();
+    //  Exclude files which look like they might be in node_modules...
+    if (file.match(rexNodeModules)) {
+      debug(`skipping '${file}' as it appears to be in a node_modules folder...`);
+      return false;
+    }
+
+    //  When checking to see if a file is in the 'build' folder, we only want
+    //  to check the _relative_ path - it is OK if the search root or parent
+    //  path includes 'build'.
+    const relativeFilePath = path.relative(absoluteSearchRoot, file);
+
+    //  Exclude files which look like they might be in android build intermediate
+    //  folders...
+    if (relativeFilePath.match(rexBuildFolder)) {
+      debug(`skipping '${file}' as it appears to be in an android build folder...`);
+      return false;
+    }
+
+    return true;
   });
 };
