c73f7355fb680dc7959fd78c9944b13c2ee06527
bencergazda <benedek.kaldi@srg.hu>
2019-08-15 12:44:37 +0200

    feat: Strip metadata from output images

--- src/resize/resize-image.js
+++ src/resize/resize-image.js
@@ -2,5 +2,5 @@ const imagemagickCli = require('imagemagick-cli');
 
 //  Takes a source image, resizes to a target path.
 module.exports = async function resizeImage(source, target, size) {
-  return imagemagickCli.exec(`convert "${source}" -resize ${size} "${target}"`);
+  return imagemagickCli.exec(`convert "${source}" -resize ${size} -strip "${target}"`);
 };
